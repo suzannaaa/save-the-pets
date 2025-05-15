1dd2149d5a93c067664ceea6653acd0e3c47d904
suzanna <suzannatan09@gmail.com>
2025-05-16 02:45:57 +0800

    fix(server): install cors for deployment

--- client/js/getCurrentURL.js
+++ client/js/getCurrentURL.js
@@ -1,5 +1,7 @@
 // getCurrentURL.js
 
 // To get the current URL and store it as currentURL for reusability
-const currentUrl = window.location.protocol + "//" + window.location.host;
+// const currentUrl = window.location.protocol + "//" + window.location.host;
+
+const currentUrl = "https://save-the-pets.onrender.com";
 console.log("currentUrl:", currentUrl);
